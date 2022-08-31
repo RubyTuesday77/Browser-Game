079d606f1598aeb95f1821e34cf7b5ad83f902d0
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2022-08-30 21:52:20 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById("canvas");
+
+let context = canvas.getContext("2d")
+
+var window_height = window.innerHeight;
+var window_width = window.innerWidth;
+
+canvas.width = window_width;
+canvas.width = window_height;
+
