c20c3d28ef09abba4860a66698d199e378f79783
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2022-09-10 00:38:20 -0400

    9/10 commit

--- start-game2.js
+++ start-game2.js
@@ -51,10 +51,28 @@ function shuffleArray(array) {
     return array
 };
 
-//IMPORTANT!!!!
 // Assign a new variable to shuffled array
 let letters = shuffleArray(letterArray);
-console.log(letters)
+
+// Assign variables to the last three items (which are arrays) returned in the gameWordArray:
+let sixLetterAnswers = gameWordArray[1];
+let fiveLetterAnswers = gameWordArray[2];
+let fourLetterAnswers = gameWordArray[3];
+
+// Assign a variable for each answer group above to add to word-box-container:
+let fourLetterList = document.getElementById('four-letter-answers');
+let fiveLetterList = document.getElementById('five-letter-answers');
+let sixLetterList = document.getElementById('six-letter-answers');
+
+// Function passes two paramenters to create a column for each group of answers
+function addAnswerColumn(list, answers) {
+    for(let i = 0; i < answers.length; i++) {
+        let answer = answers[i];
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(answer));
+        list.appendChild(li)
+    }
+}
 
 // Add event listener to start button to start round
 startButton.addEventListener("click", startRound);
@@ -69,15 +87,124 @@ function startRound(event) {
         ballArray[i].append(letterButton);
         letterButtonArray.push(letterButton);
     }
+    addAnswerColumn(fourLetterList, fourLetterAnswers);
+    addAnswerColumn(fiveLetterList, fiveLetterAnswers);
+    addAnswerColumn(sixLetterList, sixLetterAnswers);
+
     this.disabled = true;
 }
 
-
-
 function moveLetter(event) {
-    if (this.parentElement.matches('.letter-ball')) {
-        boxArray[0].append(this);
-      } else {
-        ballArray[0].append(this);
-      }
+    const div1 = document.getElementById('letter-1');
+    const div2 = document.getElementById('letter-2');
+    const div3 = document.getElementById('letter-3');
+    const div4 = document.getElementById('letter-4');
+    const div5 = document.getElementById('letter-5');
+    const div6 = document.getElementById('letter-6');
+    if(div1.innerHTML.trim() == "") {
+        div1.innerHTML = this.innerHTML;
+        this.innerHTML = "";
+    } else if(div2.innerHTML.trim() == "") {
+        div2.innerHTML = this.innerHTML;
+       this.innerHTML =  "";
+    } else if(div3.innerHTML.trim() == "") {
+        div3.innerHTML = this.innerHTML;
+        this.innerHTML = "";
+    }  else if(div4.innerHTML.trim() == "") {
+        div4.innerHTML = this.innerHTML;
+        this.innerHTML = "";
+    }  else if(div5.innerHTML.trim() == "") {
+        div5.innerHTML = this.innerHTML;
+        this.innerHTML = "";
+    }  else if(div6.innerHTML.trim() == "") {
+        div6.innerHTML = this.innerHTML;
+        this.innerHTML = "";
     }
+}
+
+// Shuffle letters
+function shuffleLetters() {
+
+}
+
+// Clear board
+
+function matchWord(wordResult) {
+    if(wordResult.length === 4) {
+        // Check if word matches any in <ol id="four-letter-answers"></ol>
+        // Grab elements from ordered list containing the same word
+        const answers = document.getElementById("four-letter-answers").getElementsByTagName("li");
+        for(let i = 0; i< answers.length; i++) {
+            if(answers[i].innerHTML == wordResult) {
+                answers[i].style.visibilty = "visible";
+                alert('You Are Right!!')
+            }
+        }
+    } else if(wordResult.length == 5) {
+        // check if word matches any in <ol id="five-letter-answers"></ol>
+        // Grab elements from ordered list containing the same word
+        const answers = document.getElementById("five-letter-answers").getElementsByTagName("li");
+        for(let i = 0; i< answers.length; i++) {
+            if(answers[i].innerHTML == wordResult) {
+                answers[i].style.visibilty = "visible";
+                alert('You Are Right!!')
+            }
+        }
+    } else if(wordResult.length  == 6) { 
+        // check if word matches any in <ol id="six-letter-answers"></ol>
+        // Grab elements from ordered list containing the same word
+        const answers = document.getElementById("six-letter-answers").getElementsByTagName("li");
+        for(let i = 0; i< answers.length; i++) {
+            if(answers[i].innerHTML == wordResult) {
+                answers[i].style.visibilty = "visible";
+                alert('You Are Right!!')
+            }
+        }
+    } else {
+        alert('WRONG')
+    }
+}
+
+// Submit word as answer
+let submitWord = document.getElementById("control-enter");
+submitWord.addEventListener("click", function getAnswer() {
+    let div1 = document.getElementsByClassName("letter-box")[0];
+    let text1 = div1.textContent;
+    let div2 = document.getElementsByClassName("letter-box")[1];
+    let text2 = div2.textContent;
+    let div3 = document.getElementsByClassName("letter-box")[2];
+    let text3 = div3.textContent;
+    let div4 = document.getElementsByClassName("letter-box")[3];
+    let text4 = div4.textContent;
+    let div5 = document.getElementsByClassName("letter-box")[4];
+    let text5 = div5.textContent;
+    let div6 = document.getElementsByClassName("letter-box")[5];
+    let text6 = div6.textContent;
+    let wordResult = [text1, text2, text3, text4, text5, text6].join("");
+    matchWord(wordResult);
+    // return wordResult;
+    console.log(wordResult); // this returns 'NARY'
+})
+
+
+// let letterSolArray = ['A','D','O']
+   // console.log(letterSolArray.join(""))
+/*
+let letterText = $('.word-solutions div').map(function(el) {
+        return el.innerText
+    })
+    console.log(letterText)
+    let letterText1 =
+
+
+    if(x.length === 4) {
+        console.log('4letters')
+    } else if(x.length == 5) {
+        console.log('5letters')
+    } else if(x.length  == 6) {
+        console.log('6letters')
+    } else {
+        console.log('error')
+    }
+
+*/
